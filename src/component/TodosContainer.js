83908c992c4c7e7818b8be52e7904e402a954317
HaeunShim <haeun62433@gmail.com>
2022-07-07 18:01:16 +0900

    update

--- src/component/TodosContainer.js
+++ src/component/TodosContainer.js
@@ -5,10 +5,11 @@ function TodoList(){
     const [ todos, setTodos ] = useState([]) // 할 일을 의미하는 배열
 
     const [ todo, setTodo ] = useState("") // input에 써져있는 내용
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        setTodos([...todos, todo])
+            setTodos([...todos, todo])
+            setTodo("");
+        
     }
     const sort = () => {
         let copy = [...todos];
@@ -16,14 +17,24 @@ function TodoList(){
         copy.sort();
         setTodos(copy);
     }
+
+    const onKeyPress = (e) => {
+        console.log(e.key)
+        if (e.key == "Enter"){
+            handleSubmit(e);
+       }
+        
+    }
+
     return(
         <>
         <header>
-            해야 할 업무 ()
+            해야 할 업무 ({todos.length})
         </header>
             <input type="text" placeholder="업무내용을 적어주세요"
-            value={todo}
-            onChange={e => setTodo(e.target.value)} />
+            value={todo} 
+            onChange={e=>setTodo(e.target.value)}
+            onKeyDown={onKeyPress} />
             <button onClick={handleSubmit}>Add To Do</button>
             <br></br>
             <button onClick = {sort} type='list'>정렬</button>
